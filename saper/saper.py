a41ebb9a0a50ffdb185c6d798a30e8e621833015
andreikin <andreikin@mail.ru>
2020-12-17 16:28:51 +0300

    add method "add mines"

--- saper/saper.py
+++ saper/saper.py
@@ -12,16 +12,24 @@ class Game:
         self.mode = mode
         self.width = conf["FIELD_SIZE"][self.mode][0]
         self.height = conf["FIELD_SIZE"][self.mode][1]
+        # define matrix with 0 values
         self.matrix = [[0 for i in range(self.width+2)] for j in range(self.height+2)]
-        self.number_of_mines = int(self.width*self.height/100*conf["MINES_PERCENTAGE"])
+        self.mines_num = int(self.width*self.height/100*conf["MINES_PERCENTAGE"])
+        self.__add_mines()
 
     def __add_mines(self):
-        pass
+        self.mines_list = []
+        while len(self.mines_list)<self.mines_num:
+            min_pos = (randint(1, self.width), randint(1, self.height))
+            if not min_pos in self.mines_list:
+                self.mines_list.append(min_pos)
+                self.matrix[min_pos[1]][min_pos[0]] = '*'
 
     def __str__(self):
         for i in self.matrix:
             print(* i)
-        print(self.number_of_mines)
+        print(self.mines_num)
+        print(len(self.mines_list))
 
 
 class Cell:
