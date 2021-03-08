fe846aab7fabc65c3c297ff83cbd43822464111d
andreikin <andreikin@mail.ru>
2021-03-08 10:35:11 +0300

    add tmp path to saving data base file

--- saper/result_handler.py
+++ saper/result_handler.py
@@ -3,30 +3,34 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import sqlite3 as sq
+import tempfile
+import os
+
 
 class ResultHandler:
     def __init__(self, mode="SMALL"):
         with open('conf.json', 'r') as file:
             self.conf = json.load(file)
+        self.db_path = os.path.join(tempfile.gettempdir(), self.conf["DATA_BASE"])
         self.mode = mode
 
     def get_win_list(self, mode):
-        with sq.connect(self.conf["DATA_BASE"]) as con:
+        with sq.connect(self.db_path) as con:
             cur = con.cursor()
             cur.execute('CREATE TABLE IF NOT EXISTS records' + mode + ' ( name Text, result Text )')
-            data = cur.execute('SELECT name, result FROM records'+mode+' ORDER BY result LIMIT 10')
+            data = cur.execute('SELECT name, result FROM records' + mode + ' ORDER BY result LIMIT 10')
         return data.fetchall()
 
     def add_result(self, obj, time, mode):
         text, ok = QInputDialog.getText(obj, 'Text Input Dialog', self.conf["WIN_TEXT"])
         if ok and text:
-            with sq.connect(self.conf["DATA_BASE"]) as con:
+            with sq.connect(self.db_path) as con:
                 cur = con.cursor()
-                cur.execute('CREATE TABLE IF NOT EXISTS records'+mode+' ( name Text, result Text )')
-                cur.execute('INSERT INTO records'+mode+' VALUES ("' + text + '", "' + time + '")')
+                cur.execute('CREATE TABLE IF NOT EXISTS records' + mode + ' ( name Text, result Text )')
+                cur.execute('INSERT INTO records' + mode + ' VALUES ("' + text + '", "' + time + '")')
 
     def erase_records(self):
-        with sq.connect(self.conf["DATA_BASE"]) as con:
+        with sq.connect(self.db_path) as con:
             cur = con.cursor()
             cur.execute('DROP TABLE   records' + self.mode)
 
@@ -34,6 +38,7 @@ class ResultHandler:
         records_data = self.get_win_list(self.mode)
         records_dealog = ResultDialog(records_data)
         records_dealog.exec_()
+
 
 class ResultDialog(QDialog):
     def __init__(self, records_data):
@@ -54,3 +59,6 @@ class ResultDialog(QDialog):
         self.cencel_btn = QPushButton("Cancel")
         self.layout.addWidget(self.cencel_btn)
         self.cencel_btn.clicked.connect(self.reject)
+
+
+ResultHandler()
